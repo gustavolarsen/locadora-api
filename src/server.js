eef5913ca08071f5684a3db99b67d91580e6749f
Gustavo Larsen <guslarsen@gmail.com>
2021-03-27 14:15:00 -0300

    Adicionado middlewares de segurança

--- src/server.js
+++ src/server.js
@@ -1,14 +1,16 @@
-const express = require('express');
-
 require('dotenv').config();
 
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
 const { router } = require('./routes.js');
 
 const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(express.json());
-
+app.use(cors());
+app.use(helmet());
 app.use(router);
 
 if (process.env.NODE_ENV !== 'test') {
